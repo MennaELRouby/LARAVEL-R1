c32da0561ee6c5b0d34d5beb662304c6e9a9ff3b
MennaELRouby <roubymenna@gmail.com>
2024-01-14 00:38:15 +0200

    15th  task

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -75,6 +75,11 @@
                             </div>
                         </div>
 
+                        <div class="col-md-6">
+                            <a href="{{ route('facebook') }}">Login with
+                                facebook</a>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
